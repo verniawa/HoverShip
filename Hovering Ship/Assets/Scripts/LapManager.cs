480ac32159483d1444a5e9fb7527cbe5c9f43192
William Vernia <verniawilly@gmail.com>
2020-11-30 14:05:01 -0500

    Added UI and Lap Timer
    this commit breaks ship selection temporarily

---
+++ Hovering Ship/Assets/Scripts/LapManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapManager : MonoBehaviour
+{
+    private Transform checkpointsParent;
+    private int lastCheckpointPassed;
+    private float timeStamp;
+    public float currentLapTime {get; private set; } = 0;
+
+    public float lastLapTime  {get; private set; } = 0;
+    private int checkpointsCount;
+    private int checkpointLayer;
+    public int currentLap {get; private set; } = 0;
+    public float bestLapTime {get; private set; } = Mathf.Infinity / 2;
+
+
+    //This code is based off a tutorial series by Imphenzia
+    //"Low Poly Racing" series https://www.youtube.com/watch?v=h-sEtELln9k 
+    
+
+    private void Awake() {
+        checkpointsParent = GameObject.Find("Checkpoints").transform;
+        checkpointsCount = checkpointsParent.childCount;
+        checkpointLayer = LayerMask.NameToLayer("Checkpoint");
+    }
+
+    void startLap(){
+        // Debug.Log("start lap");
+        currentLap++;
+        lastCheckpointPassed = 0;
+        timeStamp = Time.time;
+    }
+
+    void endLap(){
+        lastLapTime = currentLapTime;
+        bestLapTime = Mathf.Min(bestLapTime, currentLapTime);   
+        currentLapTime = 0;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.layer == checkpointLayer){
+            if(other.gameObject.name == "0"){
+                if(lastCheckpointPassed == checkpointsCount - 1){
+                    endLap();
+                }
+                if (currentLap == 0 || lastCheckpointPassed == checkpointsCount - 1){
+                    startLap();
+                }
+            }
+            if(other.gameObject.name ==(lastCheckpointPassed + 1).ToString()){
+                lastCheckpointPassed++;
+            }
+
+
+        }
+    }
+
+    void Update(){
+        if (currentLap > 0){
+            currentLapTime = Time.time - timeStamp;
+        }
+
+    }
+}
---
+++ Hovering Ship/Assets/Scripts/UIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject UIPanel;
+    public TextMeshProUGUI UIcurrentLap;
+    public TextMeshProUGUI UICurrentLapTime;
+    public TextMeshProUGUI UIlastLapTime;
+    public TextMeshProUGUI UIbestTime;
+
+    public LapManager updateUIForPlayer;
+    public Rigidbody rigidbody;
+    public TextMeshProUGUI UISpeed;
+
+    private int currentLap = -1;
+    private float currentLapTime, lastLapTime, bestTime;
+    private int speed;
+
+    void Update(){
+        if(updateUIForPlayer.currentLap != currentLap){
+            currentLap = updateUIForPlayer.currentLap;
+            UIcurrentLap.text = $"Lap: {currentLap}";
+        }
+        if (updateUIForPlayer.lastLapTime != lastLapTime){
+            lastLapTime = updateUIForPlayer.lastLapTime;
+            UIlastLapTime.text = $"Previous: {(int)lastLapTime / 60}:{lastLapTime % 60:00.000}";
+        }
+        if (updateUIForPlayer.currentLapTime != currentLapTime){
+            currentLapTime = updateUIForPlayer.currentLapTime;
+            UICurrentLapTime.text = $"Time: {(int)currentLapTime / 60}:{currentLapTime % 60:00.000}";
+        }
+        if (updateUIForPlayer.bestLapTime != bestTime && updateUIForPlayer.bestLapTime < Mathf.Infinity){
+            bestTime = updateUIForPlayer.bestLapTime;
+            UIbestTime.text = $"Best: {(int)bestTime / 60}:{bestTime % 60:00.000}";
+        }
+        if ((int) rigidbody.velocity.magnitude != speed){
+            speed = (int) rigidbody.velocity.magnitude;
+            UISpeed.text = $"Speed: {speed}";
+        }
+    }
+}
