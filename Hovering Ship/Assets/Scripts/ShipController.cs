144900cbed3d9a427ea2e6398b7ab82877b0c9b8
William Vernia <verniawa@vcu.edu>
2021-05-18 17:46:39 -0400

    Separated player input from ship controller

--- Hovering Ship/Assets/Scripts/ShipController.cs
+++ Hovering Ship/Assets/Scripts/ShipController.cs
@@ -2,118 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipController : MonoBehaviour{
-    Rigidbody rigidbody;
-    public Transform shipBody;
-    public Ship ship;
-    public float thrust, speed;
-    public float topSpeed;
-    public float handling, bodyRotation, pitchRotation;
+public class ShipController : MonoBehaviour {
+    
+
+    #region Input Vars
+    private bool accel, leftBrake, rightBrake;
+    private float steer, pitch;
+    #endregion
+
+    // this is how we will interface with the class
+    public void setInputs(bool accel, float steer, float pitch, bool leftBrake, bool rightBrake){
+        this.accel = accel;
+        this.steer = steer;
+        this.pitch = pitch;
+        this.leftBrake = leftBrake;
+        this.rightBrake = rightBrake;
+    }
+
+    private float speed, speedScale = .001f;
     Vector3 startPos;
     Quaternion startRot;
-    PlayerControls controls;
-    public float airBrakeFactor = 1;
-    public GameObject leftAirBrake, rightAirBrake;
-    private float speedScale = .001f;
-    
-    private void Awake() {
-        controls = new PlayerControls();
-        controls.Gameplay.Reset.performed += ctx => resetShip();
 
+    [Header("Ship Attributes")]
+    public float topSpeed;
+    public float thrust, handling, pitchAmount;
+    [SerializeField] float bodyRotation;
+
+
+
+    #region Components
+    [SerializeField] Transform shipBody; //manual reference to ship body for now
+    Rigidbody rb;
+    #endregion
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
+        // shipBody = transform.Find("AGS");
     }
 
-    void Start(){
-        rigidbody = GetComponent<Rigidbody>();
-        startPos = rigidbody.position;
-        startRot = rigidbody.rotation;
+    private void Start() {
+        startPos = rb.position;
+        startRot = rb.rotation;
     }
-    void resetShip(){
-        rigidbody.position = startPos;
-        rigidbody.rotation = startRot;
-        rigidbody.velocity = Vector3.zero;
-    }
+
     void FixedUpdate(){
+        accelerate();
+        steering();
+        pitching();
         updatePosition();
-        if (controls.Gameplay.Accelerate.ReadValue<float>() > 0){
-            accelerate();
-        } else{
-            decelerate();
-        }
-        turning();
-        pitch();
-        if (controls.Gameplay.AirBrakeLeft.ReadValue<float>() > 0 ){
-            AirBrakeLeft();
-        }
-        if (controls.Gameplay.AirBrakeRight.ReadValue<float>() > 0){
-            AirBrakeRight();
-        }
-        
     }
 
-    //apply acceleration
+
     void accelerate(){
-        if (speed < topSpeed){
-            speed += thrust;
-        } else {
-            speed = topSpeed;
+        if (accel){    
+            if (speed < topSpeed){
+                speed += thrust;
+            } else {
+                speed = topSpeed;
+            }
+        } else{
+            speed *= .995f;
         }
     }
 
-    void decelerate(){
-        speed *= .995f;
+    void steering(){
+        float rotate = handling * steer;
+        float roll = - bodyRotation * steer;
+        Quaternion rollAngle = transform.rotation * Quaternion.Euler(0f, 0f, roll);
+        shipBody.rotation = Quaternion.Lerp(shipBody.rotation, rollAngle, Time.deltaTime * 10f);
+        rb.AddRelativeTorque(Vector3.up * rotate, ForceMode.Acceleration);
+    }
+    void pitching(){
+        Vector3 rotation = Vector3.right * (pitch * pitchAmount);
+        rb.AddRelativeTorque(rotation, ForceMode.Acceleration);
     }
 
     void updatePosition(){
-        // transform.position += speedScale * speed * transform.forward;
-        rigidbody.AddForce(speedScale * speed * transform.forward, ForceMode.VelocityChange);
-
-        float sidewaysSpeed = Vector3.Dot(rigidbody.velocity, transform.right);
+        rb.AddForce(speedScale * speed * transform.forward, ForceMode.VelocityChange);
+        float sidewaysSpeed = Vector3.Dot(rb.velocity, transform.right);
         Vector3 sideFriction = -transform.right * (sidewaysSpeed / Time.fixedDeltaTime / 2);
-        rigidbody.AddForce(sideFriction, ForceMode.Acceleration);
-    }
-    //apply turning
-    void turning(){
-        float rotate = handling * controls.Gameplay.Steering.ReadValue<float>();
-        float roll = - bodyRotation * controls.Gameplay.Steering.ReadValue<float>();
-        Quaternion rollAngle = transform.rotation * Quaternion.Euler(0f, 0f, roll);
-        
-        shipBody.rotation = Quaternion.Lerp(shipBody.rotation, rollAngle, Time.deltaTime * 10f);
-
-        Vector3 rotation = Vector3.up * rotate;
-
-        rigidbody.AddRelativeTorque(rotation, ForceMode.Acceleration);
-    
+        rb.AddForce(sideFriction, ForceMode.Acceleration);
     }
 
-    void pitch(){
-        float rotate = pitchRotation * controls.Gameplay.Pitch.ReadValue<float>();
-        Vector3 rotation = Vector3.right * rotate;
-        rigidbody.AddRelativeTorque(rotation, ForceMode.Acceleration);
+    public void resetShip(){
+        rb.position = startPos;
+        rb.rotation = startRot;
+        rb.velocity = Vector3.zero;
     }
 
-    void AirBrakeLeft(){
-        // Vector3 brakeForceLeft = - airBrakeFactor * Vector3.up;
-        // //calculate force proportional to velocity
-        // float fwdSpeed = transform.InverseTransformDirection(rigidbody.velocity).z;
-        // brakeForceLeft = brakeForceLeft * fwdSpeed;
-        // rigidbody.AddRelativeTorque(brakeForceLeft, ForceMode.Acceleration);
-        // //apply the force
-    }
-
-    void AirBrakeRight(){
-        // //apply force at wing of ship
-        // Vector3 brakeForceRight = -(rigidbody.velocity)* airBrakeFactor;
-        // //calculate force proportional to velocity
-        // rigidbody.AddForceAtPosition(brakeForceRight, rightAirBrake.transform.position, ForceMode.Acceleration);
-        // //apply the force
-        
-    }
-
-    private void OnEnable() {
-        controls.Gameplay.Enable();   
-    }
-    private void OnDisable() {
-        controls.Gameplay.Disable();
-    }
-    
 }
